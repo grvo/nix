7131f99dbbc77178ff907cc0c994d60913796295
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-09 19:03:28 -0300

    informações de assembly para diretório de android

---
+++ nix.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+// copyright (c) grvo <notgrivo@gmail.com>
+// licenciado por meio da mit license.
+//
+// veja o arquivo LICENSE na raíz do repositório para ver licença completa.
+
+using Android;
+using Android.App;
+
+// utilizado por AndroidBatteryInfo
+[assembly: UsesPermission(Manifest.Permission.BatteryStats)]
