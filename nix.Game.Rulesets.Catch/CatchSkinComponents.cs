fbdada9ded56bb2d60bb113dcac66382218fd27c
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-09 14:59:08 -0300

    capturar componentes de skin do jogo

---
+++ nix.Game.Rulesets.Catch/CatchSkinComponents.cs
@@ -0,0 +1,17 @@
+// copyright (c) grvo <notgrivo@gmail.com>
+// licenciado por meio da mit license.
+//
+// veja o arquivo LICENSE na raíz do repositório para ver licença completa.
+
+namespace nix.Game.Rulesets.Catch
+{
+    public enum CatchSkinComponents
+    {
+        Fruit,
+        Banana,
+        Droplet,
+        Catcher,
+        CatchComboCounter,
+        HitExplosion
+    }
+}
