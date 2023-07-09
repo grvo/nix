948ffb2d2f4ef7a817fbb15d60c866edf3af244f
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-09 18:55:31 -0300

    nullable removido do scheme de controle de toques

---
+++ nix.Game.Rulesets.Taiko/Configuration/TaikoTouchControlScheme.cs
@@ -0,0 +1,14 @@
+// copyright (c) grvo <notgrivo@gmail.com>
+// licenciado por meio da mit license.
+//
+// veja o arquivo LICENSE na raíz do repositório para ver licença completa.
+
+namespace nix.Game.Rulesets.Taiko.Configuration
+{
+    public enum TaikoTouchControlScheme
+    {
+        KDDK,
+        DDKK,
+        KKDD
+    }
+}
